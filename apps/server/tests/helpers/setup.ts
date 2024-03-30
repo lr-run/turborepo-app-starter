834572b6aee21f4e61ec094a7c3c4efbfff93099
rororo <16588724+rororo@users.noreply.github.com>
2024-03-30 16:08:31 +0900

    add mock clerk middleware

--- apps/server/tests/helpers/setup.ts
+++ apps/server/tests/helpers/setup.ts
@@ -1,7 +1,26 @@
-import { beforeEach } from 'vitest'
+import type { MiddlewareHandler } from 'hono'
+import { beforeEach, vi } from 'vitest'
 
 import resetDb from './reset-db'
 
+const mockedClerkMiddleware = (): MiddlewareHandler => async (c, next) => {
+  await next()
+}
+
+const mockedGetAuth = () => {
+  return {
+    userId: 'test-user-id',
+  }
+}
+
+vi.mock('@hono/clerk-auth', () => {
+  return {
+    clerkMiddleware: vi.fn(mockedClerkMiddleware),
+    getAuth: vi.fn(mockedGetAuth),
+  }
+})
+
 beforeEach(async () => {
   await resetDb()
+  vi.restoreAllMocks()
 })
