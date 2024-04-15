2c7ddd03a466b1996a05ec43cfa5be9714933f7a
rororo <16588724+rororo@users.noreply.github.com>
2024-04-15 15:41:29 +0900

    fix test script

--- apps/server/tests/helpers/reset-db.ts
+++ apps/server/tests/helpers/reset-db.ts
@@ -4,8 +4,18 @@ const prisma = new PrismaClient()
 
 export default async () => {
   console.log('---------------------- reset-db ----------------------')
-  await prisma.$transaction([
-    prisma.workspace.deleteMany(),
-    prisma.user.deleteMany(),
-  ])
+
+  const tablenames = await prisma.$queryRaw<
+    { tablename: string }[]
+  >`SELECT tablename FROM pg_tables WHERE schemaname='public'`
+
+  const tables = tablenames
+    .map(({ tablename }) => tablename)
+    .filter((name) => name !== '_prisma_migrations')
+    .map((name) => `"public"."${name}"`)
+    .join(', ')
+
+  return prisma.$executeRawUnsafe(
+    `TRUNCATE TABLE ${tables} RESTART IDENTITY CASCADE;`,
+  )
 }
